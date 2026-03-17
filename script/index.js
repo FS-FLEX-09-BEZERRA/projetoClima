80a230b7b0024534cb44f7e84035f437b945d3a1
nayaracalenzo <queiroz.14@hotmail.com>
2026-03-17 20:00:09 -0300

    CSS feito

--- script/index.js
+++ script/index.js
@@ -1,21 +1,35 @@
-
+//----------------------------------------------------------------
+// FORMAS DE CHAMAR O FETCH
 //try catch
-async function buscarAlgo () {
-  try {
-    const resposta = await fetch("url")
-    const data = await resposta.json()  //converte para objeto
-    console.log(data)
-  } catch (erro) {
-      console.error(erro)
-  }
-}
+// async function buscarAlgo () {
+//   try {
+//     const resposta = await fetch("url")
+//     const data = await resposta.json()  //converte para objeto
+//     console.log(data)
+//   } catch (erro) {
+//       console.error(erro)
+//   }
+// }
 
 //.then() .catch()
-fetch("url")
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(erro => console.error(erro))
+// fetch("url")
+//   .then(res => res.json())
+//   .then(data => console.log(data))
+//   .catch(erro => console.error(erro))
 
+//-------------------------------------------------
+ //FORMAS DE USAR ADDEVENTLISTENER
 
+// const form = document.querySelector("#form")
+//evento submit quando for formulário
+// form.addEventListener("submit", () => {
 
+// })
+
+// // evento clique quando for no botão
+// button.addEventListener("click", () => {
+
+// })
+
+//--------------------------------------------------
 
